fc4f283f43fed070276a031ef80b7814a26df8df
schrodinger <bskyy0@gmail.com>
2025-08-09 17:13:48 +0900

    ObstacleDodge

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Something hit me!");
+    }
+}
